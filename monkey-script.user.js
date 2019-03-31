379490d923d93b69802a417e446f3ae2607925c4
prixladi <el.ix@atlas.cz>
2019-03-31 20:18:38 +0200

    Update monkey-script.user.js

--- monkey-script.user.js
+++ monkey-script.user.js
@@ -8,49 +8,65 @@
 // @grant        none
 // ==/UserScript==
 
+// ==UserScript==
+// @name         Pathery extension loader
+// @namespace    https://github.com/prixladi
+// @version      0.4
+// @description  SHamyr pathery extension loader
+// @author       SHamyr
+// @match        https://www.pathery.com/*
+// @grant        none
+// ==/UserScript==
+
 const libraryId = "patheryLibraryId";
 
 const butt = document.createElement('a');
 butt.href='#';
 butt.className = "nav";
 
-const updateExtension = async () => {
-    const lib = document.getElementById(libraryId) != null;
-    lib ? removeExt(): loadExt();
-    butt.innerText = !lib ? "Remove extension" : "Add extension";
-};
-
-
 const loadExt = async () => {
     localStorage.setItem("extEnabled", true);
-    if(document.getElementById(libraryId)){
+
+    const response = await fetch('https://raw.githubusercontent.com/WuTheFWasThat/midnighttherapy/master/pathery-full.js');
+    if(!response.ok) {
+        alert('Unable to load extension, more info in console.');
+        console.log(response);
         return;
     }
 
-    const response = await fetch('https://raw.githubusercontent.com/WuTheFWasThat/midnighttherapy/master/pathery-full.js');
-    if(response.ok) {
-        const child = document.createElement('script');
-        child.id = libraryId
-        child.text = await response.text();
-        document.head.appendChild(child);
-    }
-    else {
-        alert('Unable to load extension, more info in console.');
-        console.log(response);
-    }
+    const child = document.createElement('script');
+    child.id = libraryId
+    child.text = await response.text();
+    document.head.appendChild(child);
     console.log("Pathery extension added");
 };
 
-const removeExt = async () => {
+const removeExt = (lib) => {
+    if(lib){
+        lib.parentNode.removeChild(lib);
+    }
+
     localStorage.removeItem("extEnabled");
     window.location.reload();
     console.log("Pathery extension removed.");
 };
 
+butt.addEventListener('click', async () => {
+    const lib = document.getElementById(libraryId);
+    lib ? removeExt(lib): loadExt(lib);
+    butt.innerText = !lib ? "Remove extension" : "Add extension";
+}, false);
+
+document.getElementById('topbarContent').appendChild(butt);
+
 if(localStorage.getItem("extEnabled")) {
-    loadExt();
+    if(document.getElementById(libraryId)){
+        loadExt();
+    }
+
+    butt.innerText = "Remove extension";
+}
+else {
+    butt.innerText = "Add extension";
 }
 
-butt.innerText = document.getElementById(libraryId) ? "Remove extension" : "Add extension";
-butt.addEventListener('click', updateExtension, false);
-document.getElementById('topbarContent').appendChild(butt);
