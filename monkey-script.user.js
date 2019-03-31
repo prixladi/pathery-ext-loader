81b711eb02a258a643700ec5ba1a8180b3b6dbb1
prixladi <el.ix@atlas.cz>
2019-03-31 20:01:25 +0200

    Update monkey-script.user.js

--- monkey-script.user.js
+++ monkey-script.user.js
@@ -1,25 +1,36 @@
 // ==UserScript==
 // @name         Pathery extension loader
 // @namespace    https://github.com/prixladi
-// @version      0.3
+// @version      0.4
 // @description  SHamyr pathery extension loader
 // @author       SHamyr
 // @match        https://www.pathery.com/*
 // @grant        none
 // ==/UserScript==
 
+const libraryId = "patheryLibraryId";
+
+const butt = document.createElement('a');
+butt.href='#';
+butt.className = "nav";
+
+const updateExtension = async () => {
+    const lib = document.getElementById(libraryId) != null;
+    lib ? removeExt(): loadExt();
+    butt.innerText = !lib ? "Remove extension" : "Add extension";
+};
+
 
 const loadExt = async () => {
-    const librartyId = "patheryLibrary";
-
     localStorage.setItem("extEnabled", true);
-    if(document.getElementById(librartyId))
+    if(document.getElementById(libraryId)){
         return;
+    }
 
     const response = await fetch('https://raw.githubusercontent.com/WuTheFWasThat/midnighttherapy/master/pathery-full.js');
     if(response.ok) {
         const child = document.createElement('script');
-        child.id = librartyId
+        child.id = libraryId
         child.text = await response.text();
         document.head.appendChild(child);
     }
@@ -40,23 +51,6 @@ if(localStorage.getItem("extEnabled")) {
     loadExt();
 }
 
-const loadButt = document.createElement('button');
-loadButt.className = "nav"
-loadButt.innerText = "Load";
-loadButt.style.fontSize = "0.8em";
-loadButt.style.width = "3.5em";
-loadButt.style.height = "2em";
-loadButt.style.color = 'white';
-loadButt.style.background='#000000'
-loadButt.addEventListener('click', () => loadExt(), false);
-document.getElementById('topbarContent').appendChild(loadButt);
-
-const removeButt = document.createElement('button');
-removeButt.innerText = "Remove";
-removeButt.style.fontSize = "0.8em";
-removeButt.style.width = "4.5em";
-removeButt.style.height = "2em";
-removeButt.style.color = 'white';
-removeButt.style.background='#000000'
-removeButt.addEventListener('click', () => removeExt(), false);
-document.getElementById('topbarContent').appendChild(removeButt);
+butt.innerText = document.getElementById(libraryId) ? "Remove extension" : "Add extension";
+butt.addEventListener('click', updateExtension, false);
+document.getElementById('topbarContent').appendChild(butt);
